81343592b41e96217cd93c3b065ed6387c1f0c7d
Gerasimchuk Elena <zziikk2003@gmail.com>
2022-11-27 14:40:10 +0700

    Создала описание у запросов в DishController

--- webNET-Hits-backend-aspnet-project-2/Controllers/DishController.cs
+++ webNET-Hits-backend-aspnet-project-2/Controllers/DishController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using webNET_Hits_backend_aspnet_project_2.Enums;
+using webNET_Hits_backend_aspnet_project_2.Models;
 using webNET_Hits_backend_aspnet_project_2.Models.DTO;
 using webNET_Hits_backend_aspnet_project_2.Servises.InterfacesServices;
 
@@ -7,6 +8,7 @@ namespace webNET_Hits_backend_aspnet_project_2.Controllers;
 
 [ApiController]
 [Route("api/dish")]
+[Produces("application/json")]
 public class DishController
 {
     private readonly IDishService _dishService;
@@ -16,26 +18,54 @@ public class DishController
         _dishService = dishService;
     }
 
+    /// <summary>
+    /// Get a list of dishes (menu)
+    /// </summary>
     [HttpGet]
+    [ProducesResponseType(typeof(DishPagedListDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(Response), StatusCodes.Status500InternalServerError)]
     public IEnumerable<DishDto> GetListDishes([FromQuery] List<DishCategory> categories, bool vegetarian, SortingDish sorting, int page)
     {
         return _dishService.GenerateDishes();
     }
 
+    /// <summary>
+    /// Get information about concrete dish
+    /// </summary>
     [HttpGet("{id}")]
+    [ProducesResponseType(typeof(DishDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(Response), StatusCodes.Status500InternalServerError)]
     public DishDto GetInformationConcreteDish(Guid id)
     {
         return _dishService.GetInformationAboutDish(id);
     }
-
-
+    
+    /// <summary>
+    /// Check if user is able to set ration of the dish
+    /// </summary>
     [HttpGet("{id}/rating/check")]
+    [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(Response), StatusCodes.Status500InternalServerError)]
     public string CheckCurrentUserSetRating(int id)
     {
         return id.ToString();
     }
 
+    /// <summary>
+    /// Set a rating for a dish
+    /// </summary>
     [HttpPost("{id}/rating")]
+    [ProducesResponseType(typeof(void), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(Response), StatusCodes.Status500InternalServerError)]
     public String SetRatingOfDish(int id, int ratingScore)
     {
         return "Ok";
