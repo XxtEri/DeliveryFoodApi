740eec4fbbe0863e1ca1ac59b8728464bca6eb6b
Gerasimchuk Elena <zziikk2003@gmail.com>
2022-11-27 14:48:47 +0700

    Создала описание к запросам в BasketController

--- webNET-Hits-backend-aspnet-project-2/Controllers/BasketController.cs
+++ webNET-Hits-backend-aspnet-project-2/Controllers/BasketController.cs
@@ -1,11 +1,12 @@
 using Microsoft.AspNetCore.Mvc;
 using webNET_Hits_backend_aspnet_project_2.Models;
+using webNET_Hits_backend_aspnet_project_2.Models.DTO;
 
 namespace webNET_Hits_backend_aspnet_project_2.Controllers;
 
 [ApiController]
 [Route("api/basket")]
-
+[Produces("application/json")]
 public class BasketController: ControllerBase
 {
     private readonly ApplicationDbContext _context;
@@ -15,19 +16,42 @@ public class BasketController: ControllerBase
         _context = context;
     }
     
+    /// <summary>
+    /// Get user cart
+    /// </summary>
     [HttpGet]
+    [ProducesResponseType(typeof(DishBasketDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(Response), StatusCodes.Status500InternalServerError)]
     public String GetDishesInBasket()
     {
         return "Bla";
     }
 
+    /// <summary>
+    /// Add dish to cart
+    /// </summary>
     [HttpPost("dish/{dishId}")]
+    [ProducesResponseType(typeof(void), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(Response), StatusCodes.Status500InternalServerError)]
     public String AddDishToBasket(Guid dishId)
     {
         return "Ok";
     }
 
+    /// <summary>
+    /// Decrease the number of dishes in the cart (if increase = true), ot remove the dish completely (increase = false)
+    /// </summary>
     [HttpDelete("dish/{dishId}")]
+    [ProducesResponseType(typeof(void), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status403Forbidden)]
+    [ProducesResponseType(typeof(void), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(typeof(Response), StatusCodes.Status500InternalServerError)]
     public String DeleteDishInBasket(Guid dishId, bool increase)
     {
         return "Ok";
