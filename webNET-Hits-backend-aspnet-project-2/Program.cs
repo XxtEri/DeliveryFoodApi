c5adabcc96f3aced830a1c75e6ef971f2f218318
Gerasimchuk Elena <zziikk2003@gmail.com>
2022-11-30 16:31:50 +0700

    изменила название swagger.doc

--- webNET-Hits-backend-aspnet-project-2/Program.cs
+++ webNET-Hits-backend-aspnet-project-2/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "DeliveryFood API", Version = "v1"});
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "Delivery.Кущац API", Version = "v1"});
     
     // c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
     // {
